c6dd6469fabd0acac64853a760050bc37329c24f
alanruzzo1989 <alan.ruzzo24@gmail.com>
2023-12-12 10:47:10 -0300

    arreglo pregunta metodo de pago

--- main.js
+++ main.js
@@ -18,19 +18,15 @@ if (metodoPago === 1) {
   incremento = 0.10; // Aplicar incremento del 10% para pago con tarjeta
 } else {
 
-  do {
-    metodoPago = parseInt(prompt("Ingrese el método de pago (1 para efectivo, 2 para tarjeta)"));
-    
-    if (metodoPago !== 1 && metodoPago !== 2) {
-      alert("Método de pago inválido. Por favor, ingrese el método de pago nuevamente.");
+  while (metodoPago !== 1 && metodoPago !== 2) {
+    metodoPago = parseInt(prompt("Método de pago inválido. Ingrese el método de pago nuevamente (1 para efectivo, 2 para tarjeta)"));
+    if (metodoPago === 1) {
+      descuento = 0.20; // Aplicar descuento del 20% para pago en efectivo
+    } else if (metodoPago === 2) {
+      incremento = 0.10; // Aplicar incremento del 10% para pago con tarjeta
     }
-  } while (metodoPago !== 1 && metodoPago !== 2);
+  }
 }
-
-
-
-
-
 
 let importeTotal = valorProducto - (valorProducto * descuento) + (valorProducto * incremento);
 
@@ -56,3 +52,28 @@ function despedir() {
 despedir();
 
 
+/* let descuento = 0;
+let incremento = 0;
+let respuesta = "";
+let valorProducto = parseFloat(prompt("Ingrese el valor del producto seleccionado"));
+let metodoPago = parseInt(prompt("Ingrese el método de pago (1 para efectivo, 2 para tarjeta)"));
+
+if (metodoPago === 1) {
+  descuento = 0.20; // Aplicar descuento del 20% para pago en efectivo
+} else if (metodoPago === 2) {
+  incremento = 0.10; // Aplicar incremento del 10% para pago con tarjeta
+} else {
+  while (metodoPago !== 1 && metodoPago !== 2) {
+    metodoPago = parseInt(prompt("Método de pago inválido. Ingrese el método de pago nuevamente (1 para efectivo, 2 para tarjeta)"));
+    if (metodoPago === 1) {
+      descuento = 0.20; // Aplicar descuento del 20% para pago en efectivo
+    } else if (metodoPago === 2) {
+      incremento = 0.10; // Aplicar incremento del 10% para pago con tarjeta
+    }
+  }
+}
+
+// Calcular el precio final aplicando el descuento o incremento
+let precioFinal = valorProducto * (1 - descuento + incremento);
+
+console.log("El precio final es: " + precioFinal); */
